ca10397c7605690cf58c56b473a80d268b85af2a
maduka <kossdaboss777@gmail.com>
2022-09-12 14:39:42 -0700

    here

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,13 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
+
 int main(void)
 {
 	char ch;
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
+
 		putchar(ch);
 	}
 	putchar('\n');
